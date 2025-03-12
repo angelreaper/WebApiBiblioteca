026ad38f22395bf7ed93349b6a151b86949404e5
albertloz <alberloz20@gmail.com>
2025-03-12 15:39:48 -0500

    uso de CORS y ejemplos

--- BibliotecaApi/Program.cs
+++ BibliotecaApi/Program.cs
@@ -9,6 +9,18 @@ using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var allowedOrigin = builder.Configuration.GetSection("AllowedOrigin").Get<string[]>()!;//sacamos los origenes permitidos
+builder.Services.AddCors(options => 
+    options.AddDefaultPolicy(optionCORS =>
+    {
+        //permitimos cualquier origen, cualquie invocación de metodo y cualquier header
+        optionCORS.WithOrigins(allowedOrigin)
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .WithExposedHeaders("mi-cabecera");// también permitimos la cabecera personalizada para que cors no la bloqueé
+    })
+);
 //área de servicios
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddControllers().AddNewtonsoftJson();// agregamos el newtonsoft
@@ -49,6 +61,15 @@ builder.Services.AddAuthorization (options => {
 var app = builder.Build();
 
 //área de middelwares
+
+//agregamos una cabecera personalizada
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("mi-cabecera", "valor");
+    await next();
+});
+      
+app.UseCors();// indicamos que use cors
 app.MapControllers();
 
 app.Run();
